a113035453fba73e6b884bf73d4c85fc2ec8e1b5
sudarshanr173-create <sudarshanr173@gmail.com>
2026-06-15 22:33:27 +0530

    Add files via upload

---
+++ linklist3.java
@@ -0,0 +1,19 @@
+import java.util.LinkedList;
+
+public class linklist3 {
+
+    public static void main(String args[]) {
+
+        LinkedList<Integer> ll = new LinkedList<>();
+
+        ll.addLast(1);
+        ll.addLast(2);
+        ll.addFirst(0);
+
+        System.out.println(ll);
+
+        ll.removeLast();
+
+        System.out.println(ll);
+    }
+}
