0b2df963d57a0947b5d34d00618a19b1c1a6e534
sudarshanr173-create <sudarshanr173@gmail.com>
2026-06-20 10:35:12 +0530

    Add files via upload

---
+++ backtracking3.java
@@ -0,0 +1,18 @@
+public class backtracking3 {
+    public static int gridways(int i, int j, int m, int n) {
+        if (i == n - 1 && j == m - 1) {
+            return 1;
+        } else if (i == n || j == n) {
+            return 0;
+        }
+        int w1 = gridways(i + 1, j, n, m);
+        int w2 = gridways(i, j + 1, n, m);
+        return w1 + w2;
+    }
+
+    public static void main(String args[]) {
+        int n = 3, m = 3;
+        System.out.println(gridways(0, 0, n, m));
+
+    }
+}
